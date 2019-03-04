3c6e98c0f319605de8dd744e6bba9c89b0500d25
xwklwwlf <xwklwwlf@gmail.com>
2019-03-04 09:47:01 +0800

    no message

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='xwklwwltestpackage',
+    license='Apache License 2.0',
+    version='0.0.1',
+    packages=['testpackage'],
+    zip_safe=False,
+    include_package_data=True,
+    url='https://github.com/xwklwwlf/testpackage',
+    author='xwklwwlf',
+    author_email='xwklwwlf@gmail.com',
+    description='test',
+    install_requires=[],
+    )
