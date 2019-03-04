92f38fa67ac8cb97828b41dbaca06b8001cb9673
xwklwwlf <xwklwwlf@gmail.com>
2019-03-04 13:52:42 +0800

    no message

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='xwklwwltestpackage',
     license='Apache License 2.0',
-    version='0.0.2',
+    version='0.0.3',
     # packages=['testpackage'],
     packages=find_packages(where='.', exclude=(), include=('*',)),
     zip_safe=False,
